ee05b88ad87f424540fc0bb169cb560170069ba8
Kathleen McGuire <ktmcguire@sandiego.edu>
2024-09-11 22:08:17 -0700

    Iterating to fix build

---
+++ build/precache-manifest.74d782552c83ec5c3524686b658da9af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b955ec976f4165a602ae90b4047171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "917325ba1755fd6d7dba",
+    "url": "/static/css/2.d278a156.chunk.css"
+  },
+  {
+    "revision": "1ab42f1a6d27cff21165",
+    "url": "/static/css/main.959023e7.chunk.css"
+  },
+  {
+    "revision": "917325ba1755fd6d7dba",
+    "url": "/static/js/2.3f3104b4.chunk.js"
+  },
+  {
+    "revision": "e7a80546da649d09aeaa888d8ae2ba6c",
+    "url": "/static/js/2.3f3104b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ab42f1a6d27cff21165",
+    "url": "/static/js/main.47802ce1.chunk.js"
+  },
+  {
+    "revision": "a7ffb07cabf54079ff64",
+    "url": "/static/js/runtime-main.d4672ffc.js"
+  }
+]);
